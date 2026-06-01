63f093704553c12faddbba4e223e2b2f833c5f4f
mallipedhisandeep <mallipedhisandeep@gmail.com>
2026-06-01 15:15:44 +0530

    Add files via upload

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-
 export default function SignupPage() {
   const router = useRouter()
   useEffect(() => { router.replace('/login') }, [router])
